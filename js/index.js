3e37b0fed290691c2c670376e067ea3a9dbd1cef
rifqyyusuf12 <rifqy@importir.co>
2022-02-19 18:36:19 +0700

    membuat profil html css javascript

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let fullImgBox = document.getElementById ("fullImgBox")
+let fullImg = document.getElementById ("fullImg")
+
+function openFullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic
+}
+
+function closeFullImg() {
+    fullImgBox.style.display = "none";
+}
+
+// load more logic
+
+let loadMoreBtn = document.querySelector('#load-more')
+let currentItem = 1
+
+loadMoreBtn.onclick = () =>{
+  let boxes = [...document.querySelectorAll('.box')];
+  for (var i = currentItem; i < currentItem + 1; i++){
+    boxes[i].style.display = 'inline-block';
+  }
+  currentItem += 1;
+}
